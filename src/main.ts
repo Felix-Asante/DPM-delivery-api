bc5e07dfe90587ec8c3016aa4617b2cadc070b38
felix_asantee <felix.asante@aleia.io>
2023-07-29 12:39:33 +0100

    api basic-setup

---
+++ src/main.ts
@@ -0,0 +1,34 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  // cors
+  app.enableCors();
+  // swagger
+  const options = {
+    customCss: `
+      .topbar-wrapper img {content:url(\'https://res.cloudinary.com/fedev/image/upload/v1690627890/moment/app-logo_tmonpb.jpg\'); width:140px; height:80px;}
+      .swagger-ui .topbar { background-color: white; }
+      `,
+    customSiteTitle: 'DPM Delivery API',
+    customfavIcon:
+      'https://res.cloudinary.com/fedev/image/upload/v1690627890/moment/app-logo_tmonpb.jpg',
+  };
+
+  if (process.env.NODE_ENV !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle('DPM Delivery API')
+      .setDescription('DPM Delivery Service API Documentation')
+      .setVersion('1.0')
+      .addTag('DPM Delivery')
+      .addBearerAuth()
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api/v1/doc', app, document, options);
+  }
+  // start server
+  await app.listen(process.env.PORT || 8080);
+}
+bootstrap();
