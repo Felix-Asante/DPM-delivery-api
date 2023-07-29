631591ed22c082c69a9cbe19dcc328dbd76db71e
felix_asantee <felix.asante@aleia.io>
2023-07-29 17:10:43 +0100

    basic user eps

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,14 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @ApiProperty()
+  @IsString()
+  fullName: string;
+  @ApiProperty()
+  @IsString()
+  phone: string;
+  @ApiProperty()
+  @IsString()
+  password: string;
+}
