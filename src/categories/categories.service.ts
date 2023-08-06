d915da07769f69fdbffd5b1e85f9917f82dc0f53
felix_asantee <felix.asante@aleia.io>
2023-08-06 12:19:04 +0100

    category

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,4 +1,81 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Category } from './entities/category.entity';
+import { Repository } from 'typeorm';
+import { ERRORS } from 'src/utils/errors';
+import { CreateCategoryDto } from './dto/create-category.dto';
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+  constructor(
+    @InjectRepository(Category)
+    private readonly categoryRepository: Repository<Category>,
+  ) {}
+
+  async createCategory(data: CreateCategoryDto, picture: Express.Multer.File) {
+    try {
+      return 'category created ';
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+
+  async getAllCategories() {
+    try {
+      const categories = await this.categoryRepository.find();
+      return categories;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+
+  async deleteCategory(id: string) {
+    try {
+      await this.findCategoryById(id);
+      const deleted = await this.categoryRepository.delete(id);
+      if (deleted.affected) {
+        return {
+          success: true,
+          message: 'Category deleted successfully',
+        };
+      } else {
+        return {
+          success: true,
+          message: 'Failed to delete category',
+        };
+      }
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+
+  async findCategoryBySlug(slug: string) {
+    try {
+      const category = await this.categoryRepository.findOne({
+        where: { slug },
+      });
+      if (!category) {
+        throw new NotFoundException(ERRORS.CATEGORIES.NOT_FOUND);
+      }
+      return category;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+  async findCategoryById(id: string) {
+    try {
+      const category = await this.categoryRepository.findOne({ where: { id } });
+      if (!category) {
+        throw new NotFoundException(ERRORS.CATEGORIES.NOT_FOUND);
+      }
+      return category;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+}
