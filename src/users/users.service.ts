c4734021bad970470e8f1a2dd618d0f0b01c25c7
felix asante <quamzgraphix@gmail.com>
2023-10-24 00:55:34 +0100

    signup from app

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -41,9 +41,8 @@ export class UsersService {
     private readonly bookingStatusRepository: Repository<BookingStatus>,
   ) {}
   async create(createUserDto: CreateUserDto, isPlaceAdmin = false) {
-    console.log(createUserDto)
     const user = await this.userRepository.findOne({
-      where: { phone: "0554436269" },
+      where: { phone: createUserDto.phone },
     });
 
     if (user) {
