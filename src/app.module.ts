0f22246c869505c70d3a9538d93e25a2c7af3bae
Felix-Asante <quamzgraphix@gmail.com>
2024-02-04 23:44:45 +0100

    database error fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,39 +1,38 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { DatabaseConnectionService } from 'database-connection.service';
-import { ConfigModule } from '@nestjs/config';
-import { UsersModule } from './users/users.module';
-import { Role } from './users/entities/role.entity';
 import { AuthModule } from './auth/auth.module';
-import { MessagesModule } from './messages/messages.module';
-import { JwtModule } from '@nestjs/jwt';
-import { PlacesModule } from './places/places.module';
-import { CategoriesModule } from './categories/categories.module';
-import { FilesModule } from './files/files.module';
-import { ProductsCategoryModule } from './products-category/products-category.module';
-import { ProductsModule } from './products/products.module';
-import { LikesModule } from './likes/likes.module';
-import { OffersModule } from './offers/offers.module';
-import { OfferTypes } from './offers/entities/offer-type.entity';
-import { PaymentTypeModule } from './payment-type/payment-type.module';
-import { PaymentmethodModule } from './paymentmethod/paymentmethod.module';
 import { BookingsModule } from './bookings/bookings.module';
 import { BookingStatus } from './bookings/entities/booking-status.entity';
-import { SmsService } from './sms/sms.service';
-import { PaymentService } from './payment/payment.service';
+import { CategoriesModule } from './categories/categories.module';
+import { DatabaseConnectionService } from './db/data-source.service';
+import { FilesModule } from './files/files.module';
+import { LikesModule } from './likes/likes.module';
+import { MessagesModule } from './messages/messages.module';
+import { OfferTypes } from './offers/entities/offer-type.entity';
+import { OffersModule } from './offers/offers.module';
+import { PaymentTypeModule } from './payment-type/payment-type.module';
 import { PaymentModule } from './payment/payment.module';
-import { dataSourceOption } from 'db/data-source';
+import { PaymentService } from './payment/payment.service';
+import { PaymentmethodModule } from './paymentmethod/paymentmethod.module';
+import { PlacesModule } from './places/places.module';
+import { ProductsCategoryModule } from './products-category/products-category.module';
+import { ProductsModule } from './products/products.module';
 import { ReviewsModule } from './reviews/reviews.module';
+import { SmsService } from './sms/sms.service';
+import { Role } from './users/entities/role.entity';
+import { UsersModule } from './users/users.module';
 @Module({
   imports: [
-    // TypeOrmModule.forRootAsync({
-    //   useClass: DatabaseConnectionService,
-    // }),
-    TypeOrmModule.forRoot(dataSourceOption),
-    TypeOrmModule.forFeature([Role, OfferTypes, BookingStatus]),
     ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      useClass: DatabaseConnectionService,
+    }),
+    // TypeOrmModule.forRootAsync(dataSourceOption),
+    TypeOrmModule.forFeature([Role, OfferTypes, BookingStatus]),
     JwtModule.register({
       global: true,
       secret: process.env.JWT_SECRET,
