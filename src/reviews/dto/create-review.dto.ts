fcafbedffd75b304025c6c985ad9e9570f05588f
Felix-Asante <quamzgraphix@gmail.com>
2024-03-04 13:53:57 +0100

    rating changed to decimal in dto

--- src/reviews/dto/create-review.dto.ts
+++ src/reviews/dto/create-review.dto.ts
@@ -1,12 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsInt, IsString, Max, Min } from 'class-validator';
+import { IsDecimal, IsInt, IsString, Max, Min } from 'class-validator';
 
 export class RatePlaceDto {
   @IsString()
   @ApiProperty()
   comment: string;
 
-  @IsInt()
+  @IsDecimal()
   @Max(5)
   @Min(1)
   @ApiProperty()
