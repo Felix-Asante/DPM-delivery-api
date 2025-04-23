28f109f5d869316ef198b1ec1dbf09c328fa38da
Felix-Asante <webdevwithfelix@gmail.com>
2025-04-23 15:11:09 +0100

    create shipping order

--- src/crons/crons.service.ts
+++ src/crons/crons.service.ts
@@ -15,28 +15,28 @@ export class CronsService {
 
   @Cron(CronExpression.EVERY_MINUTE)
   changeBookingPaymentStatus() {
-    tryCatch(async () => {
-      const unPaidBookings = await this.bookingService.findUnPaidBookings();
-      if (unPaidBookings.length === 0) {
-        return null;
-      }
-      unPaidBookings.map(async (booking) => {
-        const response = await this.paymentService.checkPaymentStatus({
-          requestId: booking.request_id,
-          transactionId: booking.transaction_id,
-        });
-        // @ts-ignore
-        console.log('RESPONSE===', response);
-        // @ts-ignore
-        if (!response?.error) {
-          booking.paid = true;
-          await booking.save();
-          await this.messageService.sendSmsMessage({
-            recipient: [booking.recipient_phone],
-            message: `We have received your booking. Download receipt ${booking.receipt_url}`,
-          });
-        }
-      });
-    });
+    // tryCatch(async () => {
+    //   const unPaidBookings = await this.bookingService.findUnPaidBookings();
+    //   if (unPaidBookings.length === 0) {
+    //     return null;
+    //   }
+    //   unPaidBookings.map(async (booking) => {
+    //     const response = await this.paymentService.checkPaymentStatus({
+    //       requestId: booking.request_id,
+    //       transactionId: booking.transaction_id,
+    //     });
+    //     // @ts-ignore
+    //     console.log('RESPONSE===', response);
+    //     // @ts-ignore
+    //     if (!response?.error) {
+    //       booking.paid = true;
+    //       await booking.save();
+    //       await this.messageService.sendSmsMessage({
+    //         recipient: [booking.recipient_phone],
+    //         message: `We have received your booking. Download receipt ${booking.receipt_url}`,
+    //       });
+    //     }
+    //   });
+    // });
   }
 }
